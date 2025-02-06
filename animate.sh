592377a60c05a7d038a2616dfe936722156f4a98
Jay Kickliter <jay@kickliter.com>
2025-02-05 18:06:03 -0700

    [demmit] add hill-shading

---
+++ animate.sh
@@ -0,0 +1,36 @@
+set -e
+
+cargo build --release -p demmit
+
+mkdir -p target/animate
+
+for azimuth in $(seq 0 359);
+do
+    echo $(printf "%03d" $azimuth)
+    ./target/release/demmit                 \
+        render                              \
+        --azimuth=$azimuth                  \
+        --constrain=1024                    \
+        --elevation=45                      \
+        --depth=8                           \
+        data/nasadem/1arcsecond/N38W105.hgt \
+        target/animate/$(printf "%03d" $azimuth).jpg
+done
+
+
+(
+    set +e
+
+    pushd target/animate
+
+    ffmpeg             \
+    -y                 \
+    -framerate 30      \
+    -pattern_type glob \
+    -i '*.jpg'         \
+    -c:v libx264       \
+    -pix_fmt gray      \
+    animate.mp4
+)
+
+exit $?
---
+++ demmit/src/lib.rs
@@ -0,0 +1,118 @@
+use image::{ImageBuffer, Luma, Primitive};
+use nalgebra::{DMatrix, Scalar};
+use nasadem::Tile;
+use num_traits::AsPrimitive;
+use std::f32::consts::FRAC_PI_2;
+
+pub fn tile_to_matrix<T>(tile: &Tile) -> DMatrix<T>
+where
+    T: From<i16> + Scalar,
+{
+    let (w, h) = tile.dimensions();
+    DMatrix::from_row_iterator(h, w, tile.iter().map(|sample| T::from(sample.elevation())))
+}
+
+pub fn shade(sun_az_rad: f32, sun_elev_rad: f32, data: &DMatrix<f32>) -> DMatrix<f32> {
+    let (rows, cols) = data.shape();
+    let mut out = DMatrix::zeros(rows, cols);
+    let (rows, cols) = (
+        u16::try_from(rows).expect("unexpected size"),
+        u16::try_from(cols).expect("unexpected size"),
+    );
+
+    let get = |x: i32, y: i32| {
+        let x = x.clamp(0, i32::from(cols - 1));
+        let y = y.clamp(0, i32::from(rows - 1));
+        data.index((
+            usize::try_from(y).expect("unexpected size"),
+            usize::try_from(x).expect("unexpected size"),
+        ))
+    };
+
+    for x in 0..i32::from(cols) {
+        for y in 0..i32::from(rows) {
+            let dzdx = get(x + 1, y) - get(x - 1, y);
+            let dzdy = get(x, y + 1) - get(x, y - 1);
+            let slope = (dzdx.powi(2) + dzdy.powi(2)).atan();
+            let aspect = f32::atan2(-dzdy, -dzdx);
+            let reflection = (FRAC_PI_2 - aspect - sun_az_rad).cos()
+                * (slope).sin()
+                * (FRAC_PI_2 - sun_elev_rad).sin()
+                + slope.cos() * (FRAC_PI_2 - sun_elev_rad).cos();
+            assert!(reflection.is_finite());
+            let reflection = reflection.clamp(0.2, 1.0);
+            #[allow(clippy::cast_sign_loss)]
+            {
+                *out.index_mut((y as usize, x as usize)) = reflection;
+            }
+        }
+    }
+    out
+}
+
+pub fn matrix_to_image<Pix>(data: &DMatrix<f32>) -> ImageBuffer<Luma<Pix>, Vec<Pix>>
+where
+    Pix: Primitive + Into<f32> + 'static,
+    f32: AsPrimitive<Pix>,
+{
+    let mut min_shade = f32::MAX;
+    let mut max_shade = f32::MIN;
+    let (rows, cols) = data.shape();
+    let (rows, cols) = (
+        u16::try_from(rows).expect("unexpected size"),
+        u16::try_from(cols).expect("unexpected size"),
+    );
+    let f = |col, row| {
+        let shade: f32 = *data.index((row as usize, col as usize));
+        let shade = shade
+            .round()
+            .clamp(Pix::min_value().into(), Pix::max_value().into())
+            * 256.0;
+        min_shade = min_shade.min(shade);
+        max_shade = min_shade.max(shade);
+        let shade: Pix = shade.as_();
+        Luma([shade])
+    };
+    ImageBuffer::from_fn(u32::from(cols), u32::from(rows), f)
+}
+
+#[allow(clippy::cast_precision_loss)]
+pub fn pyramid(rows: usize, cols: usize) -> DMatrix<f32> {
+    let mut out = DMatrix::zeros(rows, cols);
+    for x in 0..cols {
+        let x = if x < cols / 2 { x } else { cols - 1 - x };
+        for y in 0..rows {
+            let y = if y < rows / 2 { y } else { rows - 1 - y };
+            *out.index_mut((y, x)) = (x + y) as f32 / 4.0;
+        }
+    }
+    // dbg!(
+    //     out.get((0, 0)),
+    //     out.get((0, cols - 1)),
+    //     out.get((rows - 1, 0)),
+    //     out.get((rows - 1, cols - 1)),
+    //     out.get((rows / 2, cols / 2))
+    // );
+    out
+}
+
+#[allow(clippy::cast_precision_loss)]
+pub fn dome(rows: usize, cols: usize) -> DMatrix<f32> {
+    let mut out = DMatrix::zeros(rows, cols);
+    for x in 0..cols {
+        let xx = (x as f32 - cols as f32 / 2.0) / (cols as f32 / 2.0);
+        for y in 0..rows {
+            let yy = (y as f32 - rows as f32 / 2.0) / (rows as f32 / 2.0);
+            let elev = (1.0 - (xx.powi(2) + yy.powi(2))).sqrt() * 1600.0;
+            *out.index_mut((y, x)) = elev;
+        }
+    }
+    // dbg!(
+    //     out.get((0, 0)),
+    //     out.get((0, cols - 1)),
+    //     out.get((rows - 1, 0)),
+    //     out.get((rows - 1, cols - 1)),
+    //     out.get((rows / 2, cols / 2))
+    // );
+    out
+}
